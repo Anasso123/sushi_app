18bd66f15b56ea69da7bdd9ffa2906a8b9b7c443
anasso123 <haimeuranass4@gmail.com>
2024-01-13 21:13:48 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/anasshaimeur/sushishop/sushi_shop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.anasshaimeur.sushishop.sushi_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/model/food_category.dart
@@ -0,0 +1,9 @@
+
+import 'package:sushi_shop/src/model/food.dart';
+
+class FoodCategory {
+  final FoodType type;
+  bool isSelected;
+
+  FoodCategory(this.type, this.isSelected);
+}
